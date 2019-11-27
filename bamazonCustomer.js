66dac4646ec439843c052720eb138ddc2548cf1b
Joaquin Valle <jv346@cornell.edu>
2019-11-27 11:53:51 -0600

    added inquirer buy queries and an initial update function to the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,14 +13,15 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err
     display()
+    customerAction()
 })
+
+var products = {}
 
 function display() {
     var query = "select * from products"
     connection.query(query, (err, res) => {
-        var products = {}
         for (let i = 0; res.length > i; i++) {
-            //console.log(res[i].product_name)
             products[i] = new Product(res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity)
         }
         console.table(products)
@@ -28,8 +29,42 @@ function display() {
 }
 
 function Product(name, department, price, stock) {
-    this.Name = name
-    this.Department = department
-    this.Price = price
-    this.Stock = stock
+    this.name = name
+    this.department = department
+    this.price = price
+    this.stock = stock
+}
+
+function customerAction() {
+    inquirer.prompt([
+        {
+        name: "id",
+        message: "Enter id of the item you want to buy."
+        },
+        {
+        name: "quantity",
+        message: "Enter how many units of the product you want to buy."   
+        }
+    ]).then((response) => {
+        if (products[response.id].stock >= response.quantity) {
+
+            console.log("Enough to buy")
+        }
+        else {
+            console.log("Insufficient quantity!")
+        }
+    })
+}
+
+function customerBuy(quantity, id) {
+    var query = connection.query("update products set ? where ?",
+        [
+          {
+            stock_quantity: quantity
+        },
+        {
+            product_id: id
+          }
+        ]
+    )
 }
